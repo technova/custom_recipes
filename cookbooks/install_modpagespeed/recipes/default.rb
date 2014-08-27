fefbf7c0349803d50345a2a1e33ac0cfa56f1c3e
Rajesh Soni <rajesh.soni@saviance.com>
2014-08-27 18:22:36 +0530

    adding  cookbook for modpagespeed

--- cookbooks/install_modpagespeed/recipes/default.rb
+++ cookbooks/install_modpagespeed/recipes/default.rb
@@ -6,3 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+remote_file "#{Chef::Config[:file_cache_path]}/mod-pagespeed.deb" do
+source node['apache2']['mod_pagespeed']['package_link']
+mode '0644'
+action :create_if_missing
+end
+package 'mod_pagespeed' do
+source "#{Chef::Config[:file_cache_path]}/mod-pagespeed.deb"
+action :install
+end
+apache_module 'pagespeed' do
+conf true
+end
