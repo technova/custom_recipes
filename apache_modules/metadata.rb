e91998c5957d84169f1c06602d668c37ba17c2bd
Rajesh Soni <rajesh.soni@saviance.com>
2014-08-30 13:10:14 +0530

    adding apache modules

---
+++ apache_modules/metadata.rb
@@ -0,0 +1,8 @@
+name             'apache_modules'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures apache_modules'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+recipe 'apache_modules::mod_pagespeed', 'Apache module "pagespeed" with config file'
