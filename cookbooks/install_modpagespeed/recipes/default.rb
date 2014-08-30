1c92a19508f31a6bacd97b6be914b99915a12a02
Rajesh Soni <rajesh.soni@saviance.com>
2014-08-30 12:42:15 +0530

    remove subolic link

--- cookbooks/install_modpagespeed/recipes/default.rb
+++ cookbooks/install_modpagespeed/recipes/default.rb
@@ -11,10 +11,12 @@ source node['apache2']['mod_pagespeed']['package_link']
 mode '0644'
 action :create_if_missing
 end
+
 package 'mod_pagespeed' do
 source "#{Chef::Config[:file_cache_path]}/mod-pagespeed.deb"
 action :install
 end
+
 apache_module 'pagespeed' do
 conf true
 end
