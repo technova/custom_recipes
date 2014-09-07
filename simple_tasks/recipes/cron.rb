268a162b54289e17956a13eadbababf06e43f2c1
Rajesh Soni <rajesh.soni@saviance.com>
2014-09-07 15:19:07 +0530

    adding simple task

--- simple_tasks/recipes/cron.rb
+++ simple_tasks/recipes/cron.rb
@@ -1,5 +1,4 @@
 #
-
 # Cookbook Name:: simple
 # Recipe:: cron
 #
@@ -13,16 +12,16 @@ file log_file do
 end
 
 node[:crons].each do |cron_name, cron_data|
-  cron_d cron_name do
-    action :delete
-  end
+  # cron_d cron_name do
+  #   action :delete
+  # end
   
   env        = cron_data['environment_variables'] || ''
   cron_user   = cron_data['user'] || 'root'
 
   cron cron_name do
-    minute  cron_data['minute']
-    hour    cron_data['hour']
+    minute  cron_data['minute'] if cron_data['minute']
+    hour    cron_data['hour'] if cron_data['hour']
     weekday cron_data['weekday'] if cron_data['weekday']
     path cron_data['path'] if cron_data['path']
     mailto  cron_data['mailto'] if cron_data['mailto']
