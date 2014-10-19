c4883e63e53f6fd5b3fb9e6dc137a98a380711b6
Rajesh Soni <rajesh.soni@saviance.com>
2014-10-19 21:42:10 +0530

    adding ffmpeg

--- ffmpeg/recipes/default.rb
+++ ffmpeg/recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+template '/etc/apt/sources.list.d/mc3man-trusty-media-trusty.list' do
+  source 'mc3man-trusty-media-trusty.erb'
+  action :create_if_missing
+  notifies :run, 'execute[apt-get update]', :immediately
+end
+
+package 'ffmpeg' do
+  package_name 'ffmpeg'
+  action :install
+end
+
+package 'gstreamer0.10-ffmpeg' do
+  package_name 'gstreamer0.10-ffmpeg'
+  action :install
+end
