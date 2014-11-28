09869e70202491cdd8494a44e82f40e43655c9e8
Rajesh Soni <rajesh.soni@saviance.com>
2014-11-28 13:43:21 +0530

    adding mongo DB

---
+++ MongoDB/metadata.rb
@@ -0,0 +1,8 @@
+name             'MongoDB'
+maintainer       'Saviance'
+maintainer_email 'rajesh.soni@saviance.com'
+license          'All rights reserved'
+description      'Installs/Configures MongoDB'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends 'mongodb'
---
+++ MongoDB/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: MongoDB
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "mongodb::10gen_repo"
+include_recipe "mongodb::default"
