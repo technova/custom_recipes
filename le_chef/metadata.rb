720b2e3154b2019253a76cd684ef7316274d5130
Rajesh Soni <rajesh.soni@saviance.com>
2015-04-06 16:16:02 +0530

    adding le_chef again

---
+++ le_chef/metadata.rb
@@ -0,0 +1,18 @@
+name              'logentries_agent'
+maintainer        'Logentries'
+maintainer_email  'support@logentries.com'
+license           'Apache 2.0'
+description       'Installs and configures the logentries agent'
+long_description  'Installs the Logentries python agent and configures it to follow logs specified in a JSON object'
+recipe            'logentries_agent::default',   'Downloads the agent and sets up logging'
+recipe            'logentries_agent::install',    'Download and install the agent from le repo'
+recipe            'logentries_agent::configure', 'Register and le start agent, follow files'
+version           '0.2.0'
+
+supports 'ubuntu'
+supports 'rhel'
+
+depends 'apt'
+depends 'yum'
+
+suggests 'java', '~> 1.22'
